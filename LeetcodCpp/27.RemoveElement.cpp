0fc84636696fc3a562094b1d48bb9201e3497488
sardonicankita <67410826+sardonicankita@users.noreply.github.com>
2020-12-11 20:41:34 +0530

    Update 27.RemoveElement.cpp

--- LeetcodCpp/27.RemoveElement.cpp
+++ LeetcodCpp/27.RemoveElement.cpp
@@ -3,8 +3,8 @@ public:
     int removeElement(vector<int>& nums, int val) {
         for(int i=0; i<nums.size();i++){
             if(nums[i]==val){
-                nums.erase(nums.begin()+i);
-                i--;
+                nums.erase(nums.begin()+i);   //erasing the same element
+                i--;  
             }
         }
         return nums.size();
